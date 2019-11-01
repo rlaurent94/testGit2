332386545e6b97eea392b61d7f01e884d25eb237
laurent <r.laurent94@yahoo.fr>
2019-11-01 12:59:59 +0100

    master 12:59 commit

--- RaSeanceResumeServices.java
+++ RaSeanceResumeServices.java
@@ -20,4 +20,8 @@ public interface RaSeanceResumeServices {
 	public RaSeanceResumeData updateIntervalleFromCsv(InputStream _csvIs, RaSeanceResumeData _seanceResume) throws IOException;
     public void resetFromCsv(InputStream _csvIs) throws IOException;
     public void deleteSeanceResume(long _seanceResumeId);
+	// dupliquer pour test git depuis testGit2 12:59
+	public RaSeanceResumeData updateIntervalleFromCsv(InputStream _csvIs, RaSeanceResumeData _seanceResume) throws IOException;
+    public void resetFromCsv(InputStream _csvIs) throws IOException;
+    public void deleteSeanceResume(long _seanceResumeId);
 }
