0cbbbb5318359a935fc6a4067f0f408b75252f9a
laurent <r.laurent94@yahoo.fr>
2019-11-01 12:34:18 +0100

    branche1 12:34 commit

--- RaSeanceIntervalleServicesImpl.java
+++ RaSeanceIntervalleServicesImpl.java
@@ -11,6 +11,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+// dupliqué dans testGit2
 
 @Service
 public class RaSeanceIntervalleServicesImpl implements RaSeanceIntervalleServices {
